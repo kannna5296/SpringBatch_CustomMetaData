9a1b64e7927a166b1c0ad24b3797a3a12c9bf698
kannna5296 <kannnahacks5296@gmail.com>
2022-11-30 23:50:06 +0900

    メタデータTBLの有無によるbadSQLGrammarが出るところまで

--- src/main/kotlin/com/example/custommetadata/SampleTasklet.kt
+++ src/main/kotlin/com/example/custommetadata/SampleTasklet.kt
@@ -1,7 +1,6 @@
 package com.example.custommetadata
 
 import org.springframework.batch.core.StepContribution
-import org.springframework.batch.core.configuration.annotation.StepScope
 import org.springframework.batch.core.scope.context.ChunkContext
 import org.springframework.batch.core.step.tasklet.Tasklet
 import org.springframework.batch.repeat.RepeatStatus
@@ -16,7 +15,7 @@ class SampleTasklet : Tasklet {
 
     override fun execute(contribution: StepContribution, chunkContext: ChunkContext): RepeatStatus {
         println("SampleTaskletが起動しました")
-        jdbcTemplate.execute("SELECT 1;")
+        jdbcTemplate.execute("SELECT 1;") //仮SQL. ビジネスロジックを想定.
         return RepeatStatus.FINISHED
     }
 }
